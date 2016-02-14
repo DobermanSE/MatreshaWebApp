4276680e76232ae62083fc2307c9f80360ef09c7
alexey <ikenny404@gmail.com>
2016-02-14 12:38:22 +0300

    Added all models exept Comment, Rating, File

--- apps/store/models.py
+++ apps/store/models.py
@@ -1,154 +1,96 @@
 from django.db import models
+from django.contrib.auth.models import User
+# Необходимы для ограничения диапазона значений IntegerField
+# from django.core.validators import MinValueValidator, MaxValueValidator
 
-    # public class Category : EntityBase
-    # {
-    #     public string Name { get; set; }
-    #     public virtual Category ParentCategory { get; set; }
-    #     public virtual List<Category> SubCategories { get; set; }
-    #     public virtual List<Product> Products { get; set; }
-    #     public virtual List<ProductDetail> ProductDetails { get; set; }
-    # }
+
 # Категория
 class Category(models.Model):
-    name = models.CharField(max_length=50, null=False, help_text="Наименование категории")
+    name = models.CharField("Наименование категории", max_length=50, null=False, help_text="Наименование категории")
+    parentCategory = models.ManyToManyField("self")
 
 
-    # childCategories = models.ManyToOneRel()
-    #
-    #     /// <summary>
-    # /// Товар
-    # /// </summary>
-    # public class Product : EntityBase
-    # {
-    #     public string Name { get; set; }
-    #
-    #     public string Description { get; set; }
-    #
-    #     public virtual Category Category { get; set; }
-    #
-    #     public virtual Manufacturer Manufacturer { get; set; }
-    #
-    #     public virtual ProductStatus ProductStatus { get; set; }
-    #
-    #     public virtual ApplicationUser Seller { get; set; }
-    #
-    #     public virtual File MainImage { get; set; }
-    #
-    #     public virtual List<File> Files { get; set; }
-    #
-    #     public virtual List<Comment> Comments { get; set; }
-    #
-    #     public virtual List<Rating> Ratings { get; set; }
-    #
-    #     /// <summary>
-    #     /// Значение характеристик товара
-    #     /// </summary>
-    #     public virtual List<ProductDetailValue> ProductDetailValues { get; set; }
-    #
-    #     /// <summary>
-    #     /// Строки заказа
-    #     /// </summary>
-    #     public virtual List<OrderPosition> OrderPositions { get; set; }
-    # }
+# Производитель
+class Manufacturer(models.Model):
+    name = models.CharField("Наименование производителя", max_length=100, null=False, help_text="Наименование производителя")
+    address = models.CharField("Адрес", max_length=300, help_text="Адрес")
+    phone = models.CharField("Телефон", max_length=15, help_text="Телефон")
+    email = models.EmailField()
 
 
-    # /// <summary>
-    # /// Характеристика товара
-    # /// </summary>
-    # public class ProductDetail : EntityBase
-    # {
-    #     public string Name { get; set; }
-    #
-    #     public virtual Category Category { get; set; }
-    #
-    #     public virtual List<ProductDetailValue> ProductDetailValues { get; set; }
-    # }
+# Статус товара
+class ProductStatus(models.Model):
+    name = models.CharField("Значение статуса", max_length=50, null=False, help_text="Значение статуса")
 
 
-    #     /// <summary>
-    # /// Значение характеристики товара
-    # /// </summary>
-    # public class ProductDetailValue : EntityBase
-    # {
-    #     public string StringValue { get; set; }
-    #
-    #     public virtual Product Product { get; set; }
-    #
-    #     public virtual ProductDetail ProductDetail { get; set; }
-    # }
-
-    #     public class Rating : EntityBase
-    # {
-    #     public int Value { get; set; }
-    #
-    #     public virtual Product Product { get; set; }
-    #
-    #     public virtual ApplicationUser CreatedBy { get; set; }
-    # }
-
-    #     /// <summary>
-    # /// Строка заказа
-    # /// </summary>
-    # public class OrderPosition : EntityBase
-    # {
-    #     public int Quantity { get; set; }
-    #
-    #     public decimal Price { get; set; }
-    #
-    #     public virtual Product Product { get; set; }
-    #
-    #     public virtual Order Order { get; set; }
-    # }
+# Товар
+class Product(models.Model):
+    name = models.CharField("Наименование товара", max_length=50, null=False, help_text="Наименование товара")
+    description = models.CharField("Описание товара", max_length=50, null=False, help_text="Описание товара")
+    createdOn = models.DateTimeField("Дата добавления", auto_now_add=True)
+    category = models.ForeignKey(Category)
+    manufacturer = models.ForeignKey(Manufacturer)
+    productStatus = models.ForeignKey(ProductStatus)
+    # TODO: переделать на ImageField
+    # https://docs.djangoproject.com/es/1.9/topics/files/
+    # mainImage = models.ForeignKey(File)
+    seller = models.ForeignKey(User)
 
 
-    #     public class Order : EntityBase
-    # {
-    #     public virtual OrderStatus OrderStatus { get; set; }
-    #
-    #     public virtual ApplicationUser Customer { get; set; }
-    #
-    #     public virtual ApplicationUser Seller { get; set; }
-    #
-    #     /// <summary>
-    #     /// Строки заказа
-    #     /// </summary>
-    #     public virtual List<OrderPosition> OrderPositions { get; set; }
-    # }
+# Характеристики товара
+class ProductDetail(models.Model):
+    name = models.CharField("Наименование характеристики", max_length=50, null=False, help_text="Наименование характеристики")
+    category = models.ForeignKey(Category)
 
-    #     public class Manufacturer : EntityBase
-    # {
-    #     public string Name { get; set; }
-    #
-    #     public string Address { get; set; }
-    #
-    #     public string Phone { get; set; }
-    #
-    #     public string Email { get; set; }
-    #
-    #     public virtual List<Product> Products { get; set; }
-    # }
 
-    #    public class File : EntityBase
-    # {
-    #     [DataType(DataType.Text)]
-    #     [StringLength(100)]
-    #     [Display(Name = "Name")]
-    #     public string Name { get; set; }
-    #
-    #     public string ContentType { get; set; }
-    #
-    #     public byte[] Content { get; set; }
-    #
-    #     public virtual Product Product { get; set; }
-    #
-    #     public virtual List<Product> Products { get; set; }
-    # }
+# Значение характеристики товара
+class ProductDetailValue(models.Model):
+    stringValue = models.CharField("Строковое значение характеристики", max_length=250, help_text="Строковое значение характеристики")
+    product = models.ForeignKey(Product)
+    productDetail = models.ForeignKey(ProductDetail)
 
-    #     public class Comment : EntityBase
-    # {
-    #     public string Text { get; set; }
-    #
-    #     public virtual Product Product { get; set; }
-    #
-    #     public virtual ApplicationUser CreatedBy { get; set; }
-    # }
+
+# Статус заказа
+class OrderStatus(models.Model):
+    name = models.CharField("Статус", max_length=50, null=False, help_text="Значение статуса")
+
+
+# Заказ
+class Order(models.Model):
+    orderDate = models.DateTimeField("Дата заказа", auto_now_add=True, help_text="Дата заказа")
+    orderStatus = models.ForeignKey(OrderStatus)
+    customer = models.ForeignKey(User, related_name="customer")
+    seller = models.ForeignKey(User, related_name="seller")
+
+
+# Строка заказа
+class OrderPosition(models.Model):
+    quantity = models.IntegerField("Количество", help_text="Количество")
+    price = models.DecimalField("Стоимость", max_digits=10, decimal_places=2, help_text="Стоимость")
+    product = models.ForeignKey(Product)
+    order = models.ForeignKey(Order)
+
+
+# TODO: вынести комментарии и рейтинг в отдельное приложение (не забыть про import в начале файла)
+# Рейтинг
+# class Rating(models.Model):
+#     value = models.IntegerField("Оценка", validators=[MinValueValidator(1), MaxValueValidator(5)])
+#     createdOn = models.DateTimeField("Дата создания", auto_now_add=True)
+#     product = models.ForeignKey(Product)
+#     createdBy = models.ForeignKey(User)
+#
+#
+# # Комментарий
+# class Comment(models.Model):
+#     text = models.CharField("Стоимость", max_length=300, null=False, help_text="Текст комментария")
+#     createdOn = models.DateTimeField("Дата создания", auto_now_add=True)
+#     product = models.ForeignKey(Product)
+#     createdBy = models.ForeignKey(User)
+
+# TODO: переделать на ImageField или FilelField
+# Файл
+# class File (models.Model):
+#     name = models.CharField("Наименование файла", max_length=100, null=True, help_text="Наименование файла")
+#     contentType = models.CharField(max_length=30, null=False)
+#     content = models.BinaryField()
+#     product = models.ForeignKey(Product)
