25b718a772f1578c240e2997f10d76ee02c81045
ddzoff <dino_dzoff@mail.ru>
2016-02-16 23:36:12 +0300

    login functionallity

--- MatreshaWebApp/settings.py
+++ MatreshaWebApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.store',
+    'apps.accounts',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -125,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
--- MatreshaWebApp/urls.py
+++ MatreshaWebApp/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^store/', include('apps.store.urls')),
+    url(r'^accounts/', include('apps.accounts.urls')),
 ]
