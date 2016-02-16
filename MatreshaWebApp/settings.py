9950603b1d72a7162401397b192a59e66902e59b
alexey <ikenny404@gmail.com>
2016-02-16 22:12:44 +0300

    Added templates

--- MatreshaWebApp/settings.py
+++ MatreshaWebApp/settings.py
@@ -68,6 +68,13 @@ TEMPLATES = [
     },
 ]
 
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'templates').replace('\\', '/'),
+)
+
 WSGI_APPLICATION = 'MatreshaWebApp.wsgi.application'
 
 
