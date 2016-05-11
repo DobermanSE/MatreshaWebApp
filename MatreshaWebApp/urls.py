88ce2647951ae1a049e12d4e4a4b6edd5d78cd7a
alexey <ikenny404@gmail.com>
2016-05-11 22:34:12 +0300

    Add new design

--- MatreshaWebApp/urls.py
+++ MatreshaWebApp/urls.py
@@ -28,6 +28,6 @@ urlpatterns = [
     url(r'^accounts/', include('apps.accounts.urls')),
 ]
 
-
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
